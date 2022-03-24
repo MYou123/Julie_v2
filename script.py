b9ed194488ad43931356b7f7a10ecba3dc59fe48
MYou123 <93647864+MYou123@users.noreply.github.com>
2022-03-25 03:34:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -54,10 +54,10 @@ See <i>/help</i> for commands and more details.</b>
 
     ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @mu_si13    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Library :</b> <a href='https://rfwaofficial.com//'>Pyrogram 1.0.7</a> 
 
 """
